d96cad925b2a94c299db093f404542d69bcf2878
Mews <ar754456@gmail.com>
2024-01-01 15:09:31 +0000

    Added default configs

--- scripts/config.py
+++ scripts/config.py
@@ -14,3 +14,30 @@ def changeConfig(config, newValue):
 def getConfig(config):
     with open(FILEDIR) as f:
         return json.loads(f.read())[config]
+    
+def RESTOREDEFAULTCONFIG():
+    CONFIGS = dict()
+
+    #SPECIAL ITEM NAMES (will replace first name with second in bloxfruit.py and main.py)
+    CONFIGS["specialnames"] = [
+        ["+1 fruit storage", "fruit storage"],
+        ["2x boss drop chance", "2x boss drops"],
+        ["1 fruit storage", "fruit storage"]
+    ]
+
+    #URLS
+    CONFIGS["fruitdataurl"] = "https://blox-fruits.fandom.com/wiki/Blox_Fruits"
+    CONFIGS["gamepassdataurl"] = "https://blox-fruits.fandom.com/wiki/Shop"
+
+    CONFIGS["stockurl"] = "https://blox-fruits.fandom.com/wiki/Blox_Fruits_%22Stock%22"
+    
+    CONFIGS["tradesurl"] = "https://fruityblox.com/trading"
+
+    CONFIGS["valuesurl"] = "https://www.bloxfruitsvalues.com/"
+    CONFIGS["valuesurlappends"] = ["common", "uncommon", "rare", "legendary", "mythical", "gamepass"]
+    CONFIGS["gamepassvaluesurl"] = "https://www.bloxfruitsvalues.com/gamepass"
+
+    
+
+    with open(FILEDIR, "w") as f:
+        f.write(json.dumps(CONFIGS, indent=2))
